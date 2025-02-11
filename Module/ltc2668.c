2102b08eb3a61c503c3dfb2eac64269609eded34
xinjie@ubuntu <zjie5163@gmail.com>
2025-02-11 21:57:57 +0800

    add set voltage

--- Module/ltc2668.c
+++ Module/ltc2668.c
@@ -1,7 +1,6 @@
 #include "ltc2668.h"
 
 #include "main.h"
-//#include "stm32f1xx_hal_gpio.h"
 #include <stdint.h>
 
 /* #define CSB_Pin GPIO_PIN_12 */
@@ -22,7 +21,6 @@
 // Define a macro to set a GPIO pin to high
 #define HIGH(x) HAL_GPIO_WritePin(x##_GPIO_Port, x##_Pin, GPIO_PIN_SET)
 
-
 void SPI_Delay() {
   for (int i = 0; i < 1000; i++) {
   }
@@ -30,16 +28,25 @@ void SPI_Delay() {
 
 void LTC2668_Init() {
   HIGH(CSB);
-  //HAL_SPI_Init(&hspi2);
-  //Write code to all, update all (power up)
-  LTC2668_Write(0xa0, 0x7fff); 
+  // Write code to all, update all (power up)
+  LTC2668_Write(0xa0, 0x7fff); // Set to middle scale
 }
 
 void LTC2668_WriteChannel(uint8_t channel, uint16_t code) {
-  //Write code to n, update n (power up)
+  // Write code to n, update n (power up)
   LTC2668_Write((0x30 | channel), code);
 }
 
+void LTC2668_SetVoltage(uint8_t channel, float voltage) {
+  // Clamp the voltage
+  if (voltage > 9.9)voltage = 9.9;
+  if (voltage < -9.9)voltage = -9.9;
+  uint16_t code = (voltage + 10.0)/20.0 * 65535.0;
+  // Write code to n, update n (power up)
+  LTC2668_Write((0x30 | channel), code);
+}
+
+//Software SPI
 void LTC2668_Write(uint8_t addr, uint16_t data) {
   LOW(CSB);
 
@@ -81,13 +88,13 @@ void LTC2668_Write(uint8_t addr, uint16_t data) {
 /*         (data >> 8) & 0xFF,          // 数据高字节 */
 /*         data & 0xFF                  // 数据低字节 */
 /*     }; */
-    
+
 /*     // 手动拉低CS */
 /*     HAL_GPIO_WritePin(CS_PORT, CS_PIN, GPIO_PIN_RESET); */
-    
+
 /*     // 传输24bit数据（3个字节） */
 /*     HAL_SPI_Transmit(&hspi2, txData, 3, HAL_MAX_DELAY); */
-    
+
 /*     HAL_Delay(1); */
 /*     // 拉高CS完成传输 */
 /*     HAL_GPIO_WritePin(CS_PORT, CS_PIN, GPIO_PIN_SET); */
@@ -102,13 +109,13 @@ void LTC2668_Write(uint8_t addr, uint16_t data) {
 /*         (data >> 8) & 0xFF,       // 数据高字节 */
 /*         data & 0xFF               // 数据低字节 */
 /*     }; */
-    
+
 /*     // 手动控制CS引脚 */
 /*     HAL_GPIO_WritePin(CS_PORT, CS_PIN, GPIO_PIN_RESET); */
-    
+
 /*     // 传输32bit数据（4个字节） */
 /*     HAL_SPI_Transmit(&hspi2, txData, 4, HAL_MAX_DELAY); */
-    
+
 /*     // CS引脚置高 */
 /*     HAL_GPIO_WritePin(CS_PORT, CS_PIN, GPIO_PIN_SET); */
 /* } */
--- Module/ltc2668.h
+++ Module/ltc2668.h
@@ -5,6 +5,7 @@
 
 void LTC2668_Init();
 void LTC2668_WriteChannel(uint8_t channel, uint16_t code);
+void LTC2668_SetVoltage(uint8_t channel, float voltage);
 
 
 void LTC2668_Write(uint8_t addr, uint16_t data);
